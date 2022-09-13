67070071b09f8317958cfcfccffe367ea21d1538
ikyts18 <ikysetiawan22@gmail.com>
2022-09-13 21:14:10 +0700

    menambahkan CI PROJECT

---
+++ application/controllers/contoh_1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh_1 extends CI_Controller
+{
+	public function index(){
+		echo "Biodata <br>";
+		echo "Nama Saya; Rizki Tri Setiawan, Ibu bisa panggil saya iky, saya dari program studi sistem informasi (D3)";
+	}
+}
