92f8ccaf0644633eabe3c8d60d4a5b86ffba7d8d
Rizki Tri Setiawan <113441201+ikyts18@users.noreply.github.com>
2022-11-08 21:13:04 +0700

    Tugas UTS

--- application/views/v_data.php
+++ application/views/v_data.php
@@ -1,10 +1,15 @@
-<h1><?php echo $judul?></h1>
-<table border="1">
+<main role="main" class="container">
+    <div class="card">
+        <div class="card-header"><h4><?php echo $judul?></h4></div>
+        <div class="card-body"><a href="<?php echo base_url();?>web/create" class="btn btn-success">Tambah Data</a></div>
+    </div>
+        <table class="table table-bordered">
     <tr>
         <th>Nama</th>
         <th>Jenis Kelamin</th>
         <th>Alamat</th>
         <th>Pekerjaan</th>
+        <th>Aksi</th>
     </tr>
     <?php foreach ($bio as $b) {?>
         <tr>
@@ -12,7 +17,14 @@
             <td><?php echo $b->jenis_kelamin?></td>
             <td><?php echo $b->alamat?></td>
             <td><?php echo $b->pekerjaan?></td>
+            <td>
+                <a href="<?php echo base_url(); ?>web/edit/<?php echo $b->id; ?>" class="btn btn-warning">Edit</a>
+                <a href="<?php echo base_url();?>web/hapus/<?php echo $b->id;?>" class="btn btn-danger">Hapus</a>
+            </td>
         </tr>
+
     <?php }?>
+</main>
+
 </table>
 <br></br>
